4fd45199814b353530db20295f40d5faec0d9489
podmar <marta.podziewska@gmail.com>
2022-07-19 11:17:57 +0200

    commented out the logos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import "@fontsource/roboto";
 import CssBaseline from "@mui/material/CssBaseline";
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
