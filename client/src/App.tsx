b39bbacba41f3db7148cb0c335224c0d79b9fdf0
podmar <marta.podziewska@gmail.com>
2022-07-19 11:44:08 +0200

    imported mui theme settings, created theme and applied it, added css baseline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import "@fontsource/roboto";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles"
+import { themeLightSettings } from './utils/muiThemeSettings';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const themeLight = createTheme (themeLightSettings);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme= {themeLight}>
+    <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
