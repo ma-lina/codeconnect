62f9ae974c2ed11c0feecfa5b239d2897b68286a
podmar <marta.podziewska@gmail.com>
2022-07-19 19:29:57 +0200

    server init - commit by Lina

---
+++ server/src/server.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import { ApolloServer, gql } from "apollo-server-express";
+import { schema } from "./graphql/schema";
+import { cloudinaryConfig } from "./config/cloudinaryConfig";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+const server = new ApolloServer({ schema });
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log("Server is running on " + port + "port");
+  });
+};
+
+const loadRoutes = () => {
+  //routes
+};
+
+const middlewareSetup = () => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+  cloudinaryConfig();
+  //app.use(passport.initialize());
+  //passportConfig(passport);
+};
+
+const mongoDbConection = async () => {
+  try {
+    await mongoose.connect(process.env.DB);
+    console.log("Connection to Mongo DB established");
+  } catch (error) {
+    console.log("error connection to Mongo DB", error);
+  }
+};
+
+(async () => {
+  mongoDbConection();
+  middlewareSetup();
+  loadRoutes();
+  startServer();
+})();
