3bd1227d3fa4b65281bff9dfc97ed863836153a7
podmar <marta.podziewska@gmail.com>
2022-07-20 17:36:06 +0200

    test route in users - functional

--- server/src/routes/usersRoute.ts
+++ server/src/routes/usersRoute.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import { uploadPhoto } from "../controllers/usersController";
 import { multerUploads } from "../middleware/multer";
 
 const router = express.Router();
+
+router.get("/test", (req: Request, res: Response) => {
+    console.log("request", req)
+    res
+        .status(200)
+        .json({ msg: "Test route working." });
+});
 
 // router.post("/profile/photoUpload", jwtAuth, multerUploads.single("image"), uploadPhoto);
 router.post("/profile/photoUpload", multerUploads.single("image"), uploadPhoto);
