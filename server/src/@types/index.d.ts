98ef9c2e95e4d8e38716fa8691a1859609bb9d31
podmar <marta.podziewska@gmail.com>
2022-07-22 12:17:27 +0200

    wrote interfaces for ResponseJson and User(Registration)Data

---
+++ server/src/@types/index.d.ts
@@ -0,0 +1,21 @@
+interface ResponseJson {
+    message: string;
+    user?: UserRegistrationData;
+    token?: string;
+    image?: string;
+    error?: any;
+}
+interface UserRegistrationData {
+    first_name: string,
+    last_name: string,
+    password: string,
+    email: string,
+    user_name?: string,
+    image?: string,
+}
+interface UserData extends UserRegistrationData {
+    is_admin: boolean; 
+    starred_mentorship: any;
+    starred_coworking: any;
+    starred_shadowing: any;
+}
