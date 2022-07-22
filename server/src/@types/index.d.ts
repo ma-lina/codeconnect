ce94e865cdb3051554270828730451335a333d57
podmar <marta.podziewska@gmail.com>
2022-07-22 16:02:32 +0200

    changed the user to object in response and moved all the user interfaces to a separate file

--- server/src/@types/index.d.ts
+++ server/src/@types/index.d.ts
@@ -1,43 +1,7 @@
 interface ResponseJson {
     message: string;
-    user?: UserRegistrationData;
+    user?: object;
     token?: string;
     image?: string;
     error?: any;
-}
-
-//TODO do we need _id, timestamps passed in user data to FE?
-interface UserLoginData {
-    password: string;
-    email: string;
-}
-
-interface UserRegistrationData extends UserLoginData {
-    firstName: string;
-    lastName: string;
-    isLoggedin: boolean;
-    username?: string;
-    image?: string;
-}
-interface UserData extends UserLoginData, UserRegistrationData {
-//_id might be an object, needs converting to a string? When? 
-    _id: string;
-    isAdmin?: boolean; 
-    starredMentorship?: any;
-    starredCoworking?: any;
-    starredShadowing?: any;
-//need to add any other field coming from mongoose, like version, created and changes
-} 
-
-interface UserProfileData {
-    firstName: string;
-    lastName: string;
-    isLoggedin: boolean;
-    username?: string;
-    image?: string;
-    _id: string;
-    isAdmin?: boolean; 
-    starredMentorship?: any;
-    starredCoworking?: any;
-    starredShadowing?: any;
 }
