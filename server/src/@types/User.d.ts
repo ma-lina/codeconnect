028f3fd6061d44777e88e7ea3dbf4c7cc3d4997a
podmar <marta.podziewska@gmail.com>
2022-07-25 17:02:11 +0200

    added a names space, removed the user enums

--- server/src/@types/User.d.ts
+++ server/src/@types/User.d.ts
@@ -1,36 +1,41 @@
 //TODO do we need _id, timestamps passed in user data to FE?
 //TODO rewrite this and simplify, only need 1 or 2? 
-interface UserLoginData {
-    password: string;
-    email: string;
+declare namespace UserN {
+    interface UserLoginData {
+        password: string;
+        email: string;
+    }
+
+    interface UserRegistrationData extends UserLoginData {
+        firstName: string;
+        lastName: string;
+        isLoggedin: boolean;
+        username?: string;
+        image?: string;
+    }
+    interface UserData extends UserLoginData, UserRegistrationData {
+    //_id might be an object, needs converting to a string? When? 
+        _id: string;
+        isAdmin?: boolean; 
+        starredMentorship?: any;
+        starredCoworking?: any;
+        starredShadowing?: any;
+    //need to add any other field coming from mongoose, like version, created and changes
+    } 
+
+    interface UserProfileData {
+        firstName: string;
+        lastName: string;
+        isLoggedin: boolean;
+        username?: string;
+        image?: string;
+        _id: string;
+        isAdmin?: boolean; 
+        starredMentorship?: any;
+        starredCoworking?: any;
+        starredShadowing?: any;
+    }
 }
 
-interface UserRegistrationData extends UserLoginData {
-    firstName: string;
-    lastName: string;
-    isLoggedin: boolean;
-    username?: string;
-    image?: string;
-}
-interface UserData extends UserLoginData, UserRegistrationData {
-//_id might be an object, needs converting to a string? When? 
-    _id: string;
-    isAdmin?: boolean; 
-    starredMentorship?: any;
-    starredCoworking?: any;
-    starredShadowing?: any;
-//need to add any other field coming from mongoose, like version, created and changes
-} 
 
-interface UserProfileData {
-    firstName: string;
-    lastName: string;
-    isLoggedin: boolean;
-    username?: string;
-    image?: string;
-    _id: string;
-    isAdmin?: boolean; 
-    starredMentorship?: any;
-    starredCoworking?: any;
-    starredShadowing?: any;
-}
+
