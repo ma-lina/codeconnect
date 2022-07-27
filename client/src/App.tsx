d600ebc51855357e1d4ed6566cb05426ee441bdc
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-07-27 16:15:40 +0200

    home/logout view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { themeLightSettings } from "./utils/muiThemeSettings";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { AuthContextProvider } from "./Context/AuthContext";
+import Home from "./Views/Home";
 import Login from "./Views/Login";
 import Signup from "./Views/Signup";
 import Profile from "./Views/Profile";
@@ -18,7 +19,7 @@ function App() {
       <AuthContextProvider>
         <div className="App">
           <Routes>
-            <Route path="/" element={<App />} />
+            <Route path="*" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/profile" element={<Profile />} />
