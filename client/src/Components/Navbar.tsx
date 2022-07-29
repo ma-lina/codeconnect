db5b5aacf7e75f088bcbbf39310bb4d3581eb2d0
podmar <marta.podziewska@gmail.com>
2022-07-29 16:05:36 +0200

    created draft navbar

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import Logout from "./Logout";
 
-type Props = {};
+function Navbar() {
+  const { user, newUser } = useContext(AuthContext);
 
-function Navbar({}: Props) {
-  return <div>Navbar</div>;
+  return (
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar 
+        position="fixed" 
+        color="secondary"
+        style={{ borderRadius: "0", 
+        }}
+        >
+        <Toolbar>
+              <Typography textAlign={"left"} component="div" sx={{ flexGrow: 1 }} variant='body1'>
+                <Box
+                sx={{ fontWeight: 'bold', fontSize: 'h6.fontSize' }}
+                >
+                codeconnect
+                </Box>
+              </Typography>
+              {user && 
+                <Typography component="div" variant='body1'>
+                  <Box
+                  >
+                  {`Welcome, ${newUser.firstName} |`}
+                  </Box>
+                </Typography>
+              }
+              {user && <Logout/>}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+  
 }
 
 export default Navbar;
