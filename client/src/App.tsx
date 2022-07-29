cad452436b3a372cd863a0e47252b26faf743a65
podmar <marta.podziewska@gmail.com>
2022-07-29 15:36:33 +0200

    created and implemented protected route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import Login from "./Views/Login";
 import Signup from "./Views/Signup";
 import Profile from "./Views/Profile";
 import { Container } from "@mui/material";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
   const themeLight : Theme = createTheme(themeLightOptions);
@@ -27,7 +28,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/profile" element={<Profile />} />
+              <Route path="/profile" element={ <ProtectedRoute><Profile /></ProtectedRoute> } />
             </Routes>
           </div>
         </AuthContextProvider>
