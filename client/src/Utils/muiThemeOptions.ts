22b5895d925dacfe24f0558fa151cec7053daaa5
podmar <marta.podziewska@gmail.com>
2022-07-29 13:47:33 +0200

    capitalised utils -> Utils

---
+++ client/src/Utils/muiThemeOptions.ts
@@ -0,0 +1,21 @@
+import { ThemeOptions } from '@mui/material/styles';
+  
+  //'@material-ui/core/styles/createTheme';
+
+export const themeLightOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#2091ab',
+    },
+    secondary: {
+      main: '#b03c27',
+      light: '#F5730A',
+    },
+    error: {
+      main: '#c94b60',
+    },
+    background: {
+      paper: '#f5f5f5',
+    },
+  },
+}; 
