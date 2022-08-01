96a84d70f2f26108082bf2b76c562c1681410ac3
podmar <marta.podziewska@gmail.com>
2022-08-01 13:44:12 +0200

    added a menu button, implemented fira code as a branding text, added avatar button to log out when user logged in

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,34 +1,48 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, Toolbar, Tooltip, Typography } from "@mui/material";
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu';
 import React, { useContext } from "react";
 import { AuthContext } from "../Context/AuthContext";
 import Logout from "./Logout";
 
 function Navbar() {
-  const { user, userProfile } = useContext(AuthContext);
+  const { user, userProfile, logOut } = useContext(AuthContext);
 
   return (
         <Box sx={{ flexGrow: 1 }}>
       <AppBar 
         position="fixed" 
-        color="secondary"
+        color="primary"
         >
         <Toolbar>
-              <Typography textAlign={"left"} component="div" sx={{ flexGrow: 1 }} variant='body1'>
-                <Box
-                sx={{ fontWeight: 'bold', fontSize: 'h6.fontSize' }}
-                >
-                codeconnect
-                </Box>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography display={"flex"} textAlign={"left"} component="div" sx={{ flexGrow: 1 }} variant='h6'>
+            <Box
+              className="branding-text" 
+              sx={{ fontWeight: 'bold'}}
+              >
+              codeconnect
+            </Box>
+
+
               </Typography>
               {user && 
-                <Typography component="div" variant='body1'>
-                  <Box
-                  >
-                  {`Welcome, ${userProfile.firstName}`}
-                  </Box>
-                </Typography>
+                <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Logout">
+                  <IconButton onClick={logOut} sx={{ p: 0 }}>
+                    <Avatar alt={`Avatar of ${userProfile.firstName}`} src={userProfile.image}/>
+                  </IconButton>
+                </Tooltip>
+                </Box>
               }
-              {user && <Logout/>}
         </Toolbar>
       </AppBar>
     </Box>
