fe982390e6319c15dc7496af33fc2cfb7372197b
podmar <marta.podziewska@gmail.com>
2022-08-02 12:50:04 +0200

    added a title text

--- client/src/Views/Home.tsx
+++ client/src/Views/Home.tsx
@@ -1,7 +1,16 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Typography variant={"h3"} component={"div"}>
+        <Box className="title-text fira-code">
+          codeconnect
+        </Box>
+      </Typography>
+    </>
+  )
 }
 
 export default Home;
