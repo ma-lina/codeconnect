8dda4b782f416de95dde75e81d3b884206ebb30b
podmar <marta.podziewska@gmail.com>
2022-08-03 15:38:05 +0200

    saved server url

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const serverURL :string = "http://localhost:5000"
+
+//for deployment
+// const serverURL = process.env.NODE_ENV === "development" 
+//     ? "http://localhost:5000" 
+//     : "URLOfOurServer"
+
+export default serverURL;
