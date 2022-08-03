52aa5f983351fb5445d9f16ee7851e3d51252c1f
podmar <marta.podziewska@gmail.com>
2022-08-03 13:07:05 +0200

    new heading with styling plus buttons, alignment needs fixing

--- client/src/Views/Home.tsx
+++ client/src/Views/Home.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React, { useLayoutEffect } from "react";
-import TextHeaderLine from "../Components/TextHeaderLine";
+import ButtonHomeNavigateTo from "../Components/ButtonHomeNavigateTo";
 
 function Home() {
   useLayoutEffect(() => {
@@ -10,7 +10,15 @@ function Home() {
 
   return (
     <>
-      <TextHeaderLine text="codeconnect"/>
+      <Typography variant={"h1"} component={"div"} sx={{marginLeft:"0.8rem"}}>
+        <Box className="homepage-text fira-code cursor white-cursor">
+          codeconnect
+        </Box>
+      </Typography>
+        <Box sx={{display: 'flex', alignItems: 'center', justifyContent:"center", gap:4, p:2}}>
+          <ButtonHomeNavigateTo buttonText="Login" destination="/login"/>
+          <ButtonHomeNavigateTo buttonText="Sign up" destination="/signup"/>
+        </Box>
     </>
   )
 }
