4f5b93b8918589866add2272a523aa180b558df3
podmar <marta.podziewska@gmail.com>
2022-08-03 13:06:38 +0200

    adjusted the classnames (styling) to new naming

--- client/src/Components/TextHeaderLine.tsx
+++ client/src/Components/TextHeaderLine.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function TextHeaderLine ({ text }: TextProps) {
   return (
       <Typography variant={"h3"} component={"div"}>
-        <Box className="title-text fira-code">
+        <Box className="blue-text fira-code">
           {text}
         </Box>
       </Typography>
