18f3ad47d70c3f12ef7f293ae0cdccff71499cf6
podmar <marta.podziewska@gmail.com>
2022-08-22 12:01:37 +0200

    column layout + an edit account button

--- client/src/Components/ProfileDisplay.tsx
+++ client/src/Components/ProfileDisplay.tsx
@@ -1,34 +1,46 @@
-import { Avatar, Badge, Box, Button, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 import React, { useContext } from 'react'
 import { AuthContext } from '../Context/AuthContext'
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
 import { PhotoCamera } from "@mui/icons-material";
 
 const ProfileDisplay:React.FC = () => {
-    const { userProfile, deleteProfile } = useContext(AuthContext);
+    const { userProfile, deleteProfile } = useContext(AuthContext); 
+
 
   return (
       <>
           {userProfile &&
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>          
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap:1, flexDirection: "column", pt:4, pb:2 }}>          
                 <Avatar
                     alt={`${userProfile.firstName} ${userProfile.lastName}`}
                     src={userProfile.image}
                     sx={{ width: 50, height: 50 }}
                 />
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>          
-                    <Typography variant="h5"
-                    >
-                        {`${userProfile.firstName} ${userProfile.lastName}`}
-                    </Typography>
-                    {userProfile.username &&
-                        <Typography variant="body2">
-                            {userProfile.username}
-                        </Typography>}
-                  </Box>
-                  <Button onClick={() => deleteProfile()}>
-                      Delete user profile
-                  </Button>
+                <Typography variant="h5"
+                >
+                    {`${userProfile.firstName} ${userProfile.lastName}`}
+                </Typography>
+                {userProfile.username &&
+                    <Typography variant="body2">
+                        {userProfile.username}
+                    </Typography>}
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap:1, pt:4, pb:2 }}>          
+                    <Button 
+                    startIcon={<EditIcon/>}
+                    onClick={() => deleteProfile()}>
+                        Edit profile
+                    </Button>
+                    <Button 
+//  TODO: make notification before deleting account
+                    startIcon={<DeleteIcon/>}
+                    onClick={() => deleteProfile()}>
+                        Delete profile
+                    </Button>
+                </Box>
+
             </Box>}
       </>
   )
