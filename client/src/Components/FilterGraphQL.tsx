3943ed3a866adf1c0488b9ef171ba66a40b05e3c
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-08-24 17:11:16 +0200

    client filter mui options

--- client/src/Components/FilterGraphQL.tsx
+++ client/src/Components/FilterGraphQL.tsx
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 //import { GET_ADS } from "../GraphQL/Queries";
 import { usePinboardFilters } from "../Utils/filter";
 
@@ -27,26 +28,36 @@ function FilterGraphQL() {
 
   if (loading) return <div>Loading</div>;
   if (error) return <div>error</div>;
-  console.log(data.mentoring.length);
+
   return (
     <div>
-      <h1>
-        <a href="#albums" aria-hidden="true" id="albums">
-          <svg
-            aria-hidden="true"
-            height="16"
-            version="1.1"
-            viewBox="0 0 16 16"
-            width="16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-            ></path>
-          </svg>
-        </a>
-        Albums
-      </h1>
+      <FormControl fullWidth>
+        <InputLabel id="level">Level</InputLabel>
+        <Select
+          labelId="level"
+          id="level"
+          value={models.filters.level}
+          label="Level"
+          onChange={(e) => operations.updateFilter("level", e.target.value)}
+        >
+          <MenuItem value={"Junior"}>Junior</MenuItem>
+          <MenuItem value={"Medium"}>Twenty</MenuItem>
+          <MenuItem value={"Senior"}>Senior</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="offer">Kind of Pin</InputLabel>
+        <Select
+          labelId="offer"
+          id="offer"
+          value={models.filters.offer}
+          label="Level"
+          onChange={(e) => operations.updateFilter("request", e.target.value)}
+        >
+          <MenuItem value={false}>Request</MenuItem>
+          <MenuItem value={true}>Offer</MenuItem>
+        </Select>
+      </FormControl>
 
       <div>
         <label>Search</label>
@@ -67,7 +78,11 @@ function FilterGraphQL() {
       <button
         onClick={() =>
           refetch({
-            input: { location: models.filters.location },
+            input: {
+              location: models.filters.location,
+              level: models.filters.level,
+              offer: models.filters.offer,
+            },
           })
         }
       >
