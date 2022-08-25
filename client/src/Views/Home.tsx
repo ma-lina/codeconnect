7ba583391dbc268c293f0855807e3ddf15f64742
podmar <marta.podziewska@gmail.com>
2022-08-25 17:09:28 +0200

    removed redundant imports

--- client/src/Views/Home.tsx
+++ client/src/Views/Home.tsx
@@ -1,7 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React, { useLayoutEffect } from "react";
 import ButtonHomeNavigateTo from "../Components/ButtonHomeNavigateTo";
-import Board from "./Board";
 
 const Home: React.FC = () => {
   useLayoutEffect(() => {
