772081e4b09f5f6caa6eabaa7eea515a1644aca5
podmar <marta.podziewska@gmail.com>
2022-08-25 14:26:12 +0200

    finalised the card

--- client/src/Components/BoardCard.tsx
+++ client/src/Components/BoardCard.tsx
@@ -16,13 +16,9 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Chip, Divider, Stack } from '@mui/material';
 import { Box } from '@mui/system';
-
-const loremText = "This is a random event description text. It's the event of the year, you must join, otherwise severe FOMO. Still missing 100 signs, so I will continue writing. 40 signs left, not to bad, just a bit more."
-
 interface CardDetailProps {
   cardDetail: Query
 }
-
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
@@ -52,22 +48,13 @@ const BoardCard:React.FC<CardDetailProps> = ({cardDetail}) => {
         avatar={
           <Avatar aria-label="Avatar of the event creator"
           src={cardDetail.creator.image}
-
           >
-            
           </Avatar>
         }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="This is the pin title"
-        // title={cardDetail.title}
+        title={cardDetail.title}
         subheader={new Date(cardDetail.date).toLocaleDateString("en-GB", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
         align='left'
       />
-      {/* <Divider sx={{m:1}} variant="middle" /> */}
       <CardContent>
       <Stack flexGrow={1}  sx={{pb:2}}  direction="row" justifyContent="flex-start" alignItems="center" spacing={1}>
             <Box sx={{display:"flex", justifyContent:"flex-start", alignItems:"center" }} flexGrow={1}>
@@ -81,10 +68,6 @@ const BoardCard:React.FC<CardDetailProps> = ({cardDetail}) => {
         <Typography paragraph variant="body2" color="text.secondary" align='left'>
           {cardDetail.description.slice(0, 90) + "..."}
         </Typography>
-          {/* <Stack sx={{pb:2}}  direction="row" justifyContent="flex-end" alignItems="center" spacing={1}>
-          {cardDetail.field.map( (item, index) => (<Chip key={index} size="small" label={item}/>)
-          )}
-          </Stack> */}
           <Typography paragraph variant="body2" color="text.secondary" align='right'>
           {cardDetail.field.reduce((first, next) => first + " | " + next)}
           </Typography>
