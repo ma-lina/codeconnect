5136ee85ab0f1cb044b66ebf43bc0a5346aca8f2
podmar <marta.podziewska@gmail.com>
2022-08-25 17:10:31 +0200

    added a card for a fav item

--- client/src/Components/FavoriteItemsDisplay.tsx
+++ client/src/Components/FavoriteItemsDisplay.tsx
@@ -1,18 +1,18 @@
-import { Box, Paper, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
+import FavoriteItemCard from './FavoriteItemCard'
 
 const FavoriteItemsDisplay:React.FC = () => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', gap:1, flexDirection: "column", pb:2 }}>          
         <Typography variant={"h5"} textAlign={"center"} component={"div"} color="primary" align="left">
-            <Box className="fira-code">
+            <Box className="fira-code" textAlign="left">
             Your favourite items
             </Box>
         </Typography>
-      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap:2, py:2 }}>          
-        <Paper>Item 1</Paper>
-        <Paper>Item 2</Paper>
-        <Paper>Item 3</Paper>
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', gap:2, py:2 }}>
+        <FavoriteItemCard/>
+        <FavoriteItemCard/>
       </Box>
     </Box>
   )
