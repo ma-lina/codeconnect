b8beeb6a36820a5db72d38d5248268e7e9c73474
podmar <marta.podziewska@gmail.com>
2022-08-25 14:27:20 +0200

    removed redudndant imports

--- client/src/Components/BoardCard.tsx
+++ client/src/Components/BoardCard.tsx
@@ -2,18 +2,15 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import InsertInvitationIcon from '@mui/icons-material/InsertInvitation';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Chip, Divider, Stack } from '@mui/material';
 import { Box } from '@mui/system';
 interface CardDetailProps {
